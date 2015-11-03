3bf25ffb9f9cecffa6d75ee606ebeb812be481c6
Alec M <alecmcgovern444@gmail.com>
2015-11-03 14:37:45 -0800

    ahhhhhhh

--- controllers/quiz.js
+++ controllers/quiz.js
@@ -3,15 +3,43 @@ var router = express.Router();
 var db = require("../models");
 var request = require("request");
 
-
 router.get('/', function(req, res){
+	var songArray = [];
+	res.render('quiz', {extractStyles: true, randomSong: null, filler: null});
+});
 
+router.post('/', function(req, res){
+	res.redirect('/quiz/'+req.body.selector);
+});
+
+router.get('/:genre', function(req, res){
+	var genre = req.params.genre;
+	var songArray = [];
 	request(
-		"https://itunes.apple.com/search?term=daft+punk",
-		function(err, response, body){
+		"https://itunes.apple.com/search?term="+genre,
+		function(err, response, body) {
 			if(!err && response.statusCode === 200){
 				var data = JSON.parse(body);
-				res.render('quiz', {extractStyles: true, data: data});
+				data.results.forEach(function(song){
+					var songinfo ={};
+					songinfo.trackName = song.trackName;
+					songinfo.previewUrl = song.previewUrl;
+					songinfo.artistName = song.artistName;
+					songArray.push(songinfo);
+				});
+				var random = songArray[Math.floor(Math.random()*songArray.length)];
+				var filler = {
+					one: songArray[Math.floor(Math.random()*songArray.length)],
+					two: songArray[Math.floor(Math.random()*songArray.length)],
+					three: songArray[Math.floor(Math.random()*songArray.length)]
+				};
+				res.render('quiz', {
+					extractStyles: true, 
+					extractScripts: true, 
+					randomSong: random, 
+					filler: filler,
+					genre: genre
+				});
 			}
 		}
 	);
